ac83d8de095a5fcfb542d74b00bdd6b4c47638ff
EduMLAlmeida <edualmeidawebdev@gmail.com>
2022-01-20 15:49:48 -0300

    Atende ao requisito 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,6 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (stringInput) => ({ name: stringInput, feedback: () => 'Eita pessoa boa!' });
 
 module.exports = createStudent;
